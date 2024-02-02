6a3c9724181f447e81f69ee49cb5e42a3a040295
Michal Zander <mzander@syncad.com>
2024-02-02 11:45:27 +0000

    Remove calculate_operation_stable_id function and adjust indexes and views, calculate_operation_stable_id is not required anymore because of changes to id in hive.operations

--- db/schema.sql
+++ db/schema.sql
@@ -59,55 +59,41 @@ $$;
 
 CREATE SCHEMA IF NOT EXISTS reputation_tracker_helpers;
 
-CREATE OR REPLACE FUNCTION reputation_tracker_helpers.calculate_operation_stable_id(_block_num hive.operations.block_num%TYPE,
-  _trx_in_block hive.operations.trx_in_block%TYPE, _op_pos hive.operations.op_pos%TYPE)
-RETURNS BIGINT
-LANGUAGE 'sql'
-IMMUTABLE
-AS $BODY$
-    SELECT ((_block_num::BIGINT << 36)
-           |(CASE _trx_in_block = -1
-               WHEN TRUE THEN
-                 32768::BIGINT << 20
-               ELSE
-                 _trx_in_block::BIGINT << 20
-             END
-            )
-          | (_op_pos::bigint & '000011111111111111111111'::"bit"::BIGINT)
-          )
-    END;
-$BODY$;
 
+CREATE OR REPLACE VIEW reptracker_app.deleted_comment_operation_view AS 
+SELECT 
+  o.id,
+  o.block_num,
+  o.trx_in_block,
+  o.op_pos,
+  (o.body::jsonb -> 'value') ->> 'author' AS author,
+  (o.body::jsonb -> 'value') ->> 'permlink' AS permlink
+FROM hive.reptracker_app_operations_view o
+WHERE o.op_type_id in (17, 61); -- include delete_comment_operation and comment_payout_update_operation
 
-CREATE OR REPLACE VIEW reptracker_app.deleted_comment_operation_view
- AS
- SELECT reputation_tracker_helpers.calculate_operation_stable_id(o.block_num, o.trx_in_block, o.op_pos) AS id,
-    o.block_num,
-    o.trx_in_block,
-    o.op_pos,
-    (o.body::jsonb -> 'value'::text) ->> 'author'::text AS author,
-    (o.body::jsonb -> 'value'::text) ->> 'permlink'::text AS permlink
-   FROM hive.reptracker_app_operations_view o
-  WHERE o.op_type_id in (17, 61); -- include delete_comment_operation and comment_payout_update_operation
-
-CREATE OR REPLACE VIEW reptracker_app.hive_reputation_data_view as
-  SELECT reputation_tracker_helpers.calculate_operation_stable_id(o.block_num, o.trx_in_block, o.op_pos) AS id,
-  o.block_num, o.trx_in_block, o.op_pos, (o.body::jsonb -> 'value' ->> 'author') as author, (o.body::jsonb -> 'value' ->> 'voter') as voter,
+CREATE OR REPLACE VIEW reptracker_app.hive_reputation_data_view AS 
+SELECT 
+  o.id,
+  o.block_num, 
+  o.trx_in_block, 
+  o.op_pos, 
+  (o.body::jsonb -> 'value' ->> 'author') as author, 
+  (o.body::jsonb -> 'value' ->> 'voter') as voter,
   (o.body::jsonb -> 'value' ->> 'permlink') as permlink,
-  case jsonb_typeof (o.body::jsonb -> 'value' -> 'rshares')
-    when 'number' then
-     (o.body::jsonb -> 'value' -> 'rshares')::bigint
-  when 'string' then
-      trim(both '"' from (o.body::jsonb -> 'value' ->> 'rshares'))::bigint
-  else
-     null::bigint
-  end as rshares
+  (
+    CASE jsonb_typeof (o.body::jsonb -> 'value' -> 'rshares')
+    WHEN 'number' THEN
+     (o.body::jsonb -> 'value' -> 'rshares')::BIGINT
+    WHEN 'string' THEN
+        trim(both '"' FROM (o.body::jsonb -> 'value' ->> 'rshares'))::BIGINT
+    ELSE
+      NULL::BIGINT
+    END
+  ) AS rshares
 
-from hive.reptracker_app_operations_view o
-where o.op_type_id = 72
-  ;
-
-
+FROM hive.reptracker_app_operations_view o
+WHERE o.op_type_id = 72
+;
 
 GRANT USAGE ON SCHEMA reptracker_app TO reputation_tracker_writer_group;
 
@@ -122,7 +108,7 @@ RESET ROLE;
 CREATE INDEX IF NOT EXISTS stable_id_block_num_effective_vote_idx
     ON hive.operations USING btree
    (
-     (reputation_tracker_helpers.calculate_operation_stable_id(block_num, trx_in_block, op_pos)),
+     id,
      block_num
    )
     TABLESPACE haf_tablespace
@@ -132,10 +118,10 @@ CREATE INDEX IF NOT EXISTS stable_id_block_num_effective_vote_idx
 --- This statement must be executed by haf_block_log database owner (haf_admin)
 CREATE INDEX IF NOT EXISTS effective_comment_vote_idx ON hive.operations USING btree
     (
-   (body_binary::jsonb -> 'value' ->> 'author'::text),
-   (body_binary::jsonb -> 'value' ->> 'voter'::text),
-   (body_binary::jsonb -> 'value' ->> 'permlink'::text),
-   (reputation_tracker_helpers.calculate_operation_stable_id(block_num, trx_in_block, op_pos)) desc
+   (body_binary::jsonb -> 'value' ->> 'author'),
+   (body_binary::jsonb -> 'value' ->> 'voter'),
+   (body_binary::jsonb -> 'value' ->> 'permlink'),
+   id desc 
    )
     WHERE op_type_id = 72
   ;
@@ -143,9 +129,9 @@ CREATE INDEX IF NOT EXISTS effective_comment_vote_idx ON hive.operations USING b
 --- This statement must be executed by haf_block_log database owner (haf_admin)
 CREATE INDEX IF NOT EXISTS delete_comment_op_idx ON hive.operations USING btree
  (
-   (body_binary::jsonb -> 'value' ->> 'author'::text),
-   (body_binary::jsonb -> 'value' ->> 'permlink'::text),
-   (reputation_tracker_helpers.calculate_operation_stable_id(block_num, trx_in_block, op_pos)) desc
+   (body_binary::jsonb -> 'value' ->> 'author'),
+   (body_binary::jsonb -> 'value' ->> 'permlink'),
+   id desc
   )
   WHERE op_type_id in (17, 61)
   ;
