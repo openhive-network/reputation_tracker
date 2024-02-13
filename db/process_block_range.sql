8ea35a020f5c6a5df298b790081d3ffef1fbf2bf
Michal Zander <mzander@syncad.com>
2024-02-13 16:42:25 +0000

    CTE

--- db/process_block_range.sql
+++ db/process_block_range.sql
@@ -114,72 +114,178 @@ CREATE OR REPLACE FUNCTION reptracker_app.calculate_account_reputations_a(
     SET join_collapse_limit = 16
     SET jit = OFF
 AS $BODY$
-DECLARE
-  __account_reputations reptracker_app.AccountReputation[];
-  __author_rep bigint;
-  __new_author_rep bigint;
-  __voter_rep bigint;
-  __implicit_voter_rep boolean;
-  __implicit_author_rep boolean;
-  __rep_delta bigint;
-  __prev_rep_delta bigint;
-  __account_name varchar;
-  __prev_rshares BIGINT;
-  __rshares BIGINT;
-  __debug_log boolean := false;
 BEGIN
 
-  SELECT INTO __account_reputations
-  ARRAY(SELECT ROW(default_values.account_id, COALESCE(ad.reputation, 0), COALESCE(ad.is_implicit, true), false)::reptracker_app.AccountReputation 
+WITH selected_author_voter AS MATERIALIZED 
+( 
+  SELECT 
+    default_values.account_id, 
+    COALESCE(ad.reputation, 0) as reputation, 
+    COALESCE(ad.is_implicit, true) as is_implicit, 
+    false AS changed
   FROM 
-    (SELECT * FROM reptracker_app.account_reputations WHERE account_id = _author_id or account_id = _voter_id) AS ad
+    (
+      SELECT * FROM reptracker_app.account_reputations WHERE account_id = _author_id or account_id = _voter_id
+    ) AS ad
   RIGHT JOIN 
-    (SELECT _author_id as account_id
-	 UNION ALL 
-	 SELECT _voter_id as account_id) AS default_values
+    (
+      SELECT _author_id as account_id
+	    UNION ALL  
+	    SELECT _voter_id as account_id
+    ) AS default_values
   ON 
-    ad.account_id = default_values.account_id);
+    ad.account_id = default_values.account_id
 
-  __rshares := _rshares;
-  __prev_rshares := _prev_rshares;
-  __author_rep := __account_reputations[1].reputation;
-  __voter_rep := __account_reputations[2].reputation;
-  __implicit_author_rep := __account_reputations[1].is_implicit;
-  __implicit_voter_rep := __account_reputations[2].is_implicit;
-  __prev_rep_delta := (__prev_rshares >> 6)::bigint;
+),
+select_variables AS MATERIALIZED
+(
+  SELECT 
+    _rshares AS __rshares,
+    _prev_rshares AS __prev_rshares,
+    (_prev_rshares >> 6)::bigint AS __prev_rep_delta,
+    _author_id,
+    _author,
+    author.reputation AS __author_rep,
+    author.is_implicit AS __implicit_author_rep,
+    author.changed AS __author_changed,
+    _voter_id,
+    _voter,
+    voter.reputation AS __voter_rep,
+    voter.is_implicit AS __implicit_voter_rep
+  FROM 
+    (
+      SELECT account_id, reputation, is_implicit, changed FROM selected_author_voter WHERE account_id = _author_id LIMIT 1
+    ) AS author,
+    (
+      SELECT account_id, reputation, is_implicit, changed FROM selected_author_voter WHERE account_id = _voter_id LIMIT 1
+    ) AS voter
 
-      --- Author must have set explicit reputation to allow its correction
-  IF NOT __implicit_author_rep AND __voter_rep >= 0 AND (__prev_rshares > 0 OR
-      --- Voter must have explicitly set reputation to match hived old conditions
-      (__prev_rshares < 0 AND NOT __implicit_voter_rep AND __voter_rep > __author_rep - __prev_rep_delta)) THEN
+),
+first_condition AS MATERIALIZED 
+(
+  SELECT
+    (CASE WHEN (NOT sv.__implicit_author_rep AND sv.__voter_rep >= 0 AND (sv.__prev_rshares > 0 OR (sv.__prev_rshares < 0 AND NOT sv.__implicit_voter_rep AND sv.__voter_rep > sv.__author_rep - sv.__prev_rep_delta))) 
+    THEN true ELSE false END) AS condition,
+    sv.__rshares,
+    sv.__prev_rshares,
+    sv.__prev_rep_delta,
+    sv._author_id,
+    sv._author,
+    sv.__author_rep,
+    sv.__implicit_author_rep,
+    sv.__author_changed,
+    sv._voter_id,
+    sv._voter,
+    sv.__voter_rep,
+    sv.__implicit_voter_rep
 
-    __author_rep := __author_rep - __prev_rep_delta;
-    __implicit_author_rep := __author_rep = 0;
+  FROM select_variables sv
+),
+case_when_first AS MATERIALIZED 
+(
+  SELECT 
+    -- Voter must have explicitly set reputation to match hived old conditions
+    sv.__rshares,
+    sv.__prev_rshares,
+    sv.__prev_rep_delta,
+    sv._author_id,
+    sv._author,
+    (CASE WHEN sv.condition THEN (sv.__author_rep - sv.__prev_rep_delta) ELSE sv.__author_rep END ) AS __author_rep,
+    (CASE WHEN sv.condition THEN false ELSE sv.__implicit_author_rep END ) AS __implicit_author_rep,
+    (CASE WHEN sv.condition THEN true ELSE sv.__author_changed END ) AS __author_changed,
+    sv._voter_id,
+    sv._voter,
+    sv.__voter_rep,
+    sv.__implicit_voter_rep
 
-      IF _voter_id = _author_id THEN 
-        __implicit_voter_rep := __implicit_author_rep;
-    --- reread voter's rep. since it can change above if author == voter
-        __voter_rep := __author_rep;
-      END IF;
+  FROM first_condition sv
 
-    __account_reputations[1] := ROW(_author_id, __author_rep, __implicit_author_rep, true)::reptracker_app.AccountReputation;
-  END IF;
+),
+second_condition AS MATERIALIZED 
+(
+  SELECT
+    (CASE WHEN (sv._voter_id = sv._author_id)
+    THEN true ELSE false END) AS condition,
+    sv.__rshares,
+    sv.__prev_rshares,
+    sv.__prev_rep_delta,
+    sv._author_id,
+    sv._author,
+    sv.__author_rep,
+    sv.__implicit_author_rep,
+    sv.__author_changed,
+    sv._voter_id,
+    sv._voter,
+    sv.__voter_rep,
+    sv.__implicit_voter_rep
 
-  IF 
-  __voter_rep >= 0 AND 
-  (__rshares > 0 OR 
-  (__rshares < 0 AND NOT __implicit_voter_rep AND __voter_rep > __author_rep)) THEN
+  FROM case_when_first sv
 
-    __rep_delta := (__rshares >> 6)::bigint;
-    __new_author_rep = __author_rep + __rep_delta;
-    __account_reputations[1] := ROW(_author_id, __new_author_rep, False, true)::reptracker_app.AccountReputation;
-  END IF;
+),
+case_when_second AS MATERIALIZED 
+(
+  SELECT 
+    sv.__rshares,
+    sv.__prev_rshares,
+    sv.__prev_rep_delta,
+    sv._author_id,
+    sv._author,
+    sv.__author_rep,
+    sv.__implicit_author_rep,
+    sv.__author_changed,
+    sv._voter_id,
+    sv._voter,
+    (CASE WHEN sv.condition THEN sv.__author_rep ELSE sv.__voter_rep END ) AS __voter_rep,
+    (CASE WHEN sv.condition THEN sv.__implicit_author_rep ELSE sv.__implicit_voter_rep END ) AS __implicit_voter_rep
 
+  FROM second_condition sv
+
+),
+third_condition AS MATERIALIZED
+(
+  SELECT
+    (CASE WHEN (sv.__voter_rep >= 0 AND (sv.__rshares > 0 OR (sv.__rshares < 0 AND NOT sv.__implicit_voter_rep AND sv.__voter_rep > sv.__author_rep))) 
+    THEN true ELSE false END) AS condition,
+    sv.__rshares,
+    sv.__prev_rshares,
+    sv.__prev_rep_delta,
+    sv._author_id,
+    sv._author,
+    sv.__author_rep,
+    sv.__implicit_author_rep,
+    sv.__author_changed,
+    sv._voter_id,
+    sv._voter,
+    sv.__voter_rep,
+    sv.__implicit_voter_rep
+
+  FROM case_when_second sv
+
+),
+case_when_third AS MATERIALIZED 
+(
+  SELECT 
+    sv.__rshares,
+    sv.__prev_rshares,
+    sv.__prev_rep_delta,
+    sv._author_id,
+    sv._author,
+    (CASE WHEN sv.condition THEN (sv.__author_rep + (sv.__rshares >> 6)::bigint) ELSE sv.__author_rep END ) AS __author_rep,
+    (CASE WHEN sv.condition THEN false ELSE sv.__implicit_author_rep END ) AS __implicit_author_rep,
+    (CASE WHEN sv.condition THEN true ELSE sv.__author_changed END ) AS __author_changed,
+    sv._voter_id,
+    sv._voter,
+    sv.__voter_rep,
+    sv.__implicit_voter_rep 
+
+  FROM third_condition sv
+
+)
   INSERT INTO reptracker_app.account_reputations
     (account_id, reputation, is_implicit)
-  SELECT ds.id, ds.reputation, ds.is_implicit
-  FROM unnest(__account_reputations) ds
-  WHERE ds.reputation IS NOT NULL AND ds.changed
+  SELECT ds._author_id, ds.__author_rep, ds.__implicit_author_rep
+  FROM case_when_third ds
+  WHERE ds.__author_rep IS NOT NULL AND ds.__author_changed
   ON CONFLICT (account_id) DO UPDATE
   SET 
       reputation = EXCLUDED.reputation,
