79b1fa4ee16714f64871c468b1679013edf11788
Michal Zander <mzander@syncad.com>
2024-07-01 12:22:06 +0000

    Add openapi descriptions to reptracker

---
+++ endpoints/endpoint_schema.sql
@@ -0,0 +1,112 @@
+SET ROLE reptracker_owner;
+
+/** openapi
+openapi: 3.1.0
+info:
+  title: Reputation tracker
+  description: >-
+    Reputation tracker is an API calculating current
+    account reputation that is result of voting.
+  license:
+    name: MIT License
+    url: https://opensource.org/license/mit
+  version: 1.27.5
+externalDocs:
+  description: Reputation tracker gitlab repository
+  url: https://gitlab.syncad.com/hive/reputation_tracker
+tags:
+  - name: Account-reputation
+    description: Current account reputation 
+servers:
+  - url: /%1$s
+ */
+
+DO $__$
+DECLARE 
+  __schema_name VARCHAR;
+BEGIN
+  SHOW SEARCH_PATH INTO __schema_name;
+
+CREATE SCHEMA IF NOT EXISTS reptracker_endpoints AUTHORIZATION reptracker_owner;
+
+EXECUTE FORMAT(
+'create or replace function reptracker_endpoints.root() returns json as $_$
+declare
+-- openapi-spec
+-- openapi-generated-code-begin
+  openapi json = $$
+{
+  "openapi": "3.1.0",
+  "info": {
+    "title": "Reputation tracker",
+    "description": "Reputation tracker is an API calculating current account reputation that is result of voting.",
+    "license": {
+      "name": "MIT License",
+      "url": "https://opensource.org/license/mit"
+    },
+    "version": "1.27.5"
+  },
+  "externalDocs": {
+    "description": "Reputation tracker gitlab repository",
+    "url": "https://gitlab.syncad.com/hive/reputation_tracker"
+  },
+  "tags": [
+    {
+      "name": "Account-reputation",
+      "description": "Current account reputation"
+    }
+  ],
+  "servers": [
+    {
+      "url": "/%1$s"
+    }
+  ],
+  "paths": {
+    "/reputation/{account-name}": {
+      "get": {
+        "tags": [
+          "Account-reputation"
+        ],
+        "summary": "Account reputation",
+        "description": "Returns calculated reputation with formula found in:\nhttps://hive.blog/steemit/@digitalnotvir/how-reputation-scores-are-calculated-the-details-explained-with-simple-math\n\nSQL example\n* `SELECT * FROM reptracker_endpoints.get_account_reputation(''blocktrades'');`\n\n* `SELECT * FROM reptracker_endpoints.get_account_reputation(''initminer'');`\n\nREST call example\n* `GET https://{reptracker-host}/%1$s/reputation/blocktrades`\n\n* `GET https://{reptracker-host}/%1$s/reputation/initminer`\n",
+        "operationId": "reptracker_endpoints.get_account_reputation",
+        "parameters": [
+          {
+            "in": "path",
+            "name": "account-name",
+            "required": true,
+            "schema": {
+              "type": "string"
+            },
+            "description": "Name of the account"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "No such account in the database",
+            "content": {
+              "application/json": {
+                "schema": {
+                  "type": "integer"
+                },
+                "example": 35
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+$$;
+-- openapi-generated-code-end
+begin
+  return openapi;
+end
+$_$ language plpgsql;'
+,__schema_name);
+
+END
+$__$;
+
+RESET ROLE;
--- endpoints/get_reputation.sql
+++ endpoints/get_reputation.sql
@@ -1,15 +1,59 @@
 SET ROLE reptracker_owner;
 
-CREATE SCHEMA IF NOT EXISTS reptracker_endpoints AUTHORIZATION reptracker_owner;
+/** openapi:paths
+/reputation/{account-name}:
+  get:
+    tags:
+      - Account-reputation
+    summary: Account reputation
+    description: |
+      Returns calculated reputation with formula found in:
+      https://hive.blog/steemit/@digitalnotvir/how-reputation-scores-are-calculated-the-details-explained-with-simple-math
 
-CREATE OR REPLACE FUNCTION reptracker_endpoints.get_account_reputation(_account_id INT)
-RETURNS INT -- noqa: LT01, CP05
+      SQL example
+      * `SELECT * FROM reptracker_endpoints.get_account_reputation('blocktrades');`
+
+      * `SELECT * FROM reptracker_endpoints.get_account_reputation('initminer');`
+
+      REST call example
+      * `GET https://{reptracker-host}/%1$s/reputation/blocktrades`
+      
+      * `GET https://{reptracker-host}/%1$s/reputation/initminer`
+    operationId: reptracker_endpoints.get_account_reputation
+    parameters:
+      - in: path
+        name: account-name
+        required: true
+        schema:
+          type: string
+        description: Name of the account
+    responses:
+      '200':
+        description: |
+          Account reputation
+
+          * Returns `INT`
+        content:
+          application/json:
+            schema:
+              type: integer
+            example: 35
+        description: No such account in the database
+ */
+-- openapi-generated-code-begin
+DROP FUNCTION IF EXISTS reptracker_endpoints.get_account_reputation;
+CREATE OR REPLACE FUNCTION reptracker_endpoints.get_account_reputation(
+    "account-name" TEXT
+)
+RETURNS INT 
+-- openapi-generated-code-end
 LANGUAGE 'plpgsql' STABLE
 AS
 $$
 DECLARE
     _rep BIGINT;
     _result INT;
+    _account_id INT = (SELECT av.id FROM hive.accounts_view av WHERE av.name = "account-name");
 BEGIN
 PERFORM set_config('response.headers', '[{"Cache-Control": "public, max-age=2"}]', true);
 
