90fbb59accc4b66ef0addbac09e107e3bf758648
Jakub Ziebinski <jziebinski@gmail.com>
2025-08-08 10:02:43 +0000

    Implementation of the generated python package

---
+++ scripts/python_api_package/generate_reputation_api_client.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import sys
+from pathlib import Path
+
+from api_client_generator.rest import generate_api_client_from_swagger
+from beekeepy.handle.remote import AbstractAsyncApi
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        raise ValueError(
+            "Usage: python generate_reputation_client.py <base_directory> <build_directory>"
+        )
+
+
+    base_directory = Path(sys.argv[1])
+    build_directory = Path(sys.argv[2])
+
+    swagger_reputation_api_definition = build_directory / "swagger-doc.json"
+    reputation_api_client_output_package = base_directory / "reputation_api"
+
+    generate_api_client_from_swagger(
+        swagger_reputation_api_definition,
+        reputation_api_client_output_package,
+        AbstractAsyncApi,
+    )
